580a9f1823ff4ee798262fed917c8f96cdce579e
arjyboi <169058222+arjyboi@users.noreply.github.com>
2024-05-06 13:27:24 +0000

    Fix bugs

--- health-project/main.js
+++ health-project/main.js
@@ -5,8 +5,8 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGL3DRender({
-  canvas: document.querySelector("#bg")
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
