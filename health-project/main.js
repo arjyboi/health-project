5c7431a112da6d199b66cd9b17e6c8e795904f23
awesomearjun <iamawesomearjun@gmail.com>
2024-05-06 23:05:30 -0400

    Add No Smoking Cube and Torus

--- health-project/main.js
+++ health-project/main.js
@@ -3,7 +3,7 @@ import './style.css'
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
@@ -15,3 +15,42 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6200 });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.intensity = 150;
+pointLight.position.set(7, 5, 10);
+
+scene.add(ambientLight, pointLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+
+scene.add(lightHelper);
+
+const noSmokingTexture = new THREE.TextureLoader().load("no-smoking.webp");
+const noSmoking = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({map: noSmokingTexture}),
+);
+
+scene.add(noSmoking);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
