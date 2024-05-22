c4c49aae20fc4fe9551a42263860305d39b9da60
awesomearjun <iamawesomearjun@gmail.com>
2024-05-21 22:27:13 -0400

    Add Cube

--- health-project/main.js
+++ health-project/main.js
@@ -1,7 +1,10 @@
 import { normalMap } from 'three/examples/jsm/nodes/Nodes.js';
 import './style.css'
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 import * as THREE from 'three';
+
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -9,7 +12,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
-const background = new THREE.TextureLoader().load("space.jpg");
+const background = new THREE.TextureLoader().load("/space.jpg");
 
 scene.background = background;
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -32,7 +35,31 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 
 scene.add(ambientLight);
 
-const noSmokingTexture = new THREE.TextureLoader().load("no-smoking.webp");
+const smokingEffectsTexture = new THREE.TextureLoader().load("/smoking.jpg");
+const smokingEffects = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({map: smokingEffectsTexture}),
+);
+
+smokingEffects.position.setY(-1);
+smokingEffects.position.setX(-92);
+smokingEffects.scale.set(5, 5, 5);
+
+scene.add(smokingEffects);
+
+const questionTexture = new THREE.TextureLoader().load("/images.jpg");
+const question = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({map: questionTexture}),
+);
+
+question.position.setY(3);
+question.position.setX(-152);
+question.scale.set(3, 3, 3);
+
+scene.add(question);
+
+const noSmokingTexture = new THREE.TextureLoader().load("/no-smoking.webp");
 const noSmoking = new THREE.Mesh(
   new THREE.BoxGeometry(3, 3, 3),
   new THREE.MeshBasicMaterial({map: noSmokingTexture}),
@@ -41,8 +68,9 @@ const noSmoking = new THREE.Mesh(
 noSmoking.position.setX(25);
 noSmoking.position.setY(3);
 
-
 scene.add(noSmoking);
+
+// const controls = new OrbitControls( camera, renderer.domElement );
 
 function animate() {
   requestAnimationFrame(animate);
@@ -50,6 +78,10 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.01;
   torus.rotation.z += 0.01;
+
+  question.rotation.y += 0.01;
+
+  smokingEffects.rotation.y += 0.005;
 
   renderer.render(scene, camera);
 }
@@ -59,27 +91,17 @@ animate();
 function move() {
   let t = document.body.getBoundingClientRect().top;
 
-  camera.position.y = t * 0.11;
+  camera.position.x = t * 0.1;
 
-  noSmoking.rotation.x += 0.01;
-  noSmoking.rotation.y += 0.05;
-  noSmoking.rotation.z += 0.01;
+  noSmoking.rotation.x += 0.1;
+  noSmoking.rotation.z += 0.1;
+
+  smokingEffects.rotation.x += 0.01;
+  smokingEffects.rotation.z += 0.01;
 
   renderer.render(scene, camera); 
+  // controls.update();
 }
 
 document.onscroll = move;
 move();
-
-const noTobbaco = new THREE.TextureLoader().load("no-tobbaco.jpg");
-let noTobbacoBox = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({map: noTobbaco})
-);
-
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-
-animate();
